c217e789052c6a0571f9e3a76f5c3bbb8d75b9c1
ProgrammerCatyea <headwerted1469@gmail.com>
2025-08-31 21:05:51 -0500

    Correciones

--- src/games/games.py
+++ src/games/games.py
@@ -10,7 +10,9 @@ class Games:
             return "muy bajo"
 
     def piedra_papel_tijera(self, jugador1, jugador2):
-       
+        jugador1 = jugador1.lower()
+        jugador2 = jugador2.lower()
+
         if jugador1 == jugador2:
             return "empate"
 
@@ -26,8 +28,7 @@ class Games:
             return "jugador2"
 
     def ta_te_ti_ganador(self, tablero):
-      
-
+    
         for fila in tablero:
             if fila[0] == fila[1] == fila[2] and fila[0] != " ":
                 return fila[0]
@@ -35,7 +36,7 @@ class Games:
         for col in range(3):
             if tablero[0][col] == tablero[1][col] == tablero[2][col] and tablero[0][col] != " ":
                 return tablero[0][col]
-  
+
         if tablero[0][0] == tablero[1][1] == tablero[2][2] and tablero[0][0] != " ":
             return tablero[0][0]
 
@@ -49,21 +50,22 @@ class Games:
         return "empate"
 
     def generar_combinacion_mastermind(self, longitud, colores):
-       
         return [random.choice(colores) for _ in range(longitud)]
 
     def validar_movimiento_torre_ajedrez(self, x1, y1, x2, y2, tablero):
-        
+        if x1 == x2 and y1 == y2:
+            return False
+
         if x1 != x2 and y1 != y2:
             return False
 
-        if x1 == x2:
+        if x1 == x2:  
             paso = 1 if y2 > y1 else -1
             for y in range(y1 + paso, y2, paso):
                 if tablero[x1][y] != " ":
                     return False
 
-        elif y1 == y2:
+        elif y1 == y2: 
             paso = 1 if x2 > x1 else -1
             for x in range(x1 + paso, x2, paso):
                 if tablero[x][y1] != " ":
